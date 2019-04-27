fa779181e7ed95a6535922662fed565c7ac84e0c
Pete McLaughlin <Petemcl1978@gmail.com>
2019-04-27 09:59:40 -0400

    got it loaded, working hell no.
    looking over the JS coding some things not firing right once i get that down I have to figure out what is going on with the poor css on this. sigh a long saturday is ahead of me

--- Assetts/javascript/app.js
+++ Assetts/javascript/app.js
@@ -36,8 +36,8 @@ function animationPipeline() {
         "Martha's Vineyard is the final resting place for which SNL Alum",
         "This Late Night host was a writer on SNL and also won a writing Emmy for The Simpsons episode Marge versus The MomoRail.",
         "Boston comedian known for his roles is Canadian Bacon, Mad About You, and most noteably as 'The Guy on the couch' in Half Baked.",
-        "This pair of Boston Comedy legends appear in 'There's Something About Mary' as a Police officer and FireFighter"
-        'A Boston stand up who played a gang member with a very distinct voice turned Police officer in the Police Acadamy movies?',
+        "This pair of Boston Comedy legends appear in 'There's Something About Mary' as a Police officer and FireFighter",
+        "A Boston stand up who played a gang member with a very distinct voice turned Police officer in the Police Acadamy movies?",
         "Former Newton resident turned stand up comic wrote and directed 'Pootie Tang' and had his own show on FX.",
         "Which Late Night host and Boston Comic legend are cousins?",
     ],
@@ -71,7 +71,7 @@ function animationPipeline() {
  **/
 self.windowWasResized = function(){
     stage.style.height = (h - 20) +'px';
-    stage.style.width = (w - 20) ='px';
+    stage.style.width = (w - 20) + 'px';
 };
 /**
  * setup the stage and fire of the stage animations
@@ -87,7 +87,9 @@ self.startGamePlay = function(){
     timerSpan[0]. textContent = timerIndex;
 
     startAnimation.to([startButton, title], 1, {alpha:0});
-    startAnimation.to([startButton, title]), 0.1, {css:{display:'none'}});
+    startAnimation.to([startButton, title], 0.1, {css:{display:'none'}});
+    startAnimation.to([gameHeader,gameChoices], 0.1, {css:{display:'block'}, onComplete:self.fireOffGameLogic});
+
 };
 
 /** 
@@ -145,7 +147,7 @@ self.updateClock = function(){
  * determines if an answer is correct or incorrect
  * displays a messasge to user 
  **/
-self.anwerClicked = Function(e) {
+self.anwerClicked = function(e) {
 
     clearTimeout(timerObject);
     //Get the answer index
@@ -161,10 +163,13 @@ self.anwerClicked = Function(e) {
         cancelButtons = true;
         self.dispatch_modal('YOUR ANSWER IS: <span class="correct">CORRECT!</span>',1000);
         // Incorrect Answer
-    }  else [
-        wronganswer.play()
-        cancelButtons = true
+     }  else {
+        wronganswer.play();
+        cancelButtons = true;
         self.dispatch_modal('YOUR ANSWER IS:<span class="incorrect")>Incorrect!</span>', 1000);
+    }
+};
+
     
 /**
  * This function generates rando indexes to be used for our game logic
@@ -193,6 +198,118 @@ self.dispatch_modal = function(message, time) {
 modal_window.getElementsByTagName('p')[0].innerHTML = message;
 modal_window.style.left =((window_width / 2) - 150)+ 'px';
 
-self.fade_in(time, modal_window, true);
+self.fade_in(time, modal_window, true);{
+    var opacity = 0, interval = 50,
+    gap = interval / time, self = this;
+
+    Element.style.display = 'block';
+    Element.style.opactity = opacity;
+
+    function func() {
+    opacity += gap;
+    Elem.style.opacity = opacity;
+
+    if(opacity >= 1){
+        window.clearInterval(fading);
+        //now detect if we need to call fade out 
+        if(flag) {
+            setTimeout(function() {
+                self.fade_out(time.elem);
+        },1500);
+    }
+}
+}
+
+    var fading = window.setInterval(func,interval);
 };
 
+self.fade_out - function(time, elem) {
+    
+    function func(){
+        opacity -= gap;
+        elem.style.opacity = opacity;
+
+        if (opacity <= 0) {
+            window.clearInterval(fading);
+            elem.style.display = 'none';
+            gameIndex++;
+            //determine if nned to run another game loop
+            if(gameIndex != gameQuestions.length) {
+                timerIndex = 8;
+                timerSpan[0],textContent = timerIndexself.setupUserInterfaceWithData();
+                self.runTimer();
+            }   else {
+                self.runEndOfGame();
+            }
+        }
+    }
+        var fading = window.setInterval(func, interval);
+};
+
+/** 
+ * runs when games the game ends
+ * displays a modal window to play again
+ **/
+
+self.runEndOfGame = Function() 
+
+    window_width = window.innerWidth|| document.documentElement.clientWidth
+                    || document.body.clientWidth;
+                    
+                    var playAgainButton = '<button id="playAgain" class="left" onClick="self.resetGame()">PLAY AGAIN</button>';
+                    var actualScoreHeader = '<h2>CONGRATS, YOUR FINAL SCORE IS:'+ actualscore +',/h2>';
+                    mosal_window.getElementsByTagName('div')[0].innerHTML = insertedHTML;
+                    modal_window.style.left =((window_width /2) - 150)+ 'px';
+
+                    self.fade_in(1000,modal_window, false);
+                    };
+            // this function resets the game and starts it all over again
+
+            self.resetGame = function() {
+                modal_window.style.opacity = 0.0;
+                modal_window.innerHTML = '<div class= "modal_message"><p></p></div>';
+
+                window.clearTimeout(timerObject);
+                timerObject = undefined;
+                gameIndex = 0;
+                gameAnswers = [];
+                actualScore = 0;
+                timerIndex = 8;
+                gameQuestions = [];
+                //get the game indexes
+
+                self.generateGameIndexes();
+
+                //add data to the interface 
+                self.setupUserInterfaceWithData();
+                //set the score to zero
+                scoreSpan[0].textContent = actualScore;
+                timerSpan[0].textContent = timerIndex;
+                self.runTimer();
+            };
+
+            /**
+             * loggin Function 
+             **/
+            self.l = function(message) {
+                console.log(message);
+            };
+            
+            //initalize the functionality of the controller
+           // self._initilize();
+         //end of animationPipeline
+
+        //used to call the anumation pipeline function
+        var interval = setInterval(function() {if(document.readyState === 'complete'){
+            clearInterval(interval);
+            var pipe = animatationPipeline();
+            window.onresize = function(event) {
+                var pipe = animationPipeline()
+            };
+        }
+    },100);
+
+
+
+ 
+ 
